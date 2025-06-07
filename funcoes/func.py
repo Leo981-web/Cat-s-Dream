ef9d9f14e02faca6c4183a0db86790133d23cb39
Leo981-web <leozancanaro@outlook.com.br>
2025-06-07 20:17:46 -0300

    Adicionado sistema de colisão, melhora no sistema de pontos e adição de um abajur animado no fundo

--- funcoes/func.py
+++ funcoes/func.py
@@ -1,7 +1,13 @@
 import os 
+import pygame
 import random
 import time
+import pyttsx3
+import speech_recognition as controlador_de_voz
+recognizer = controlador_de_voz.Recognizer()
 
+def numero_aleatorio(minimo, maximo):
+    return random.randint(minimo, maximo)
 
 def limparTela():
     os.system('cls')
@@ -9,34 +15,62 @@ def limparTela():
 def aguardar(segundos):
     time.sleep(segundos)
 
-def atualiza_peixe_dourado(posicao_peixe_douradoX, posicao_peixe_douradoY, velocidade_peixe_dourado, pontos):
-    posicao_peixe_douradoY = posicao_peixe_douradoY + velocidade_peixe_dourado
-    if posicao_peixe_douradoY > 700:
+def atualiza_peixe_dourado(posicao_peixe_douradoX, posicao_peixe_douradoY, velocidade_peixe_dourado, pontos, rect_gato,):
+    rect_peixe_dourado = pygame.Rect(posicao_peixe_douradoX, posicao_peixe_douradoY, 64, 64)
+    efeito_pega_peixe = pygame.mixer.Sound("recursos/efeitoPegarPeixe.mp3")
+    if rect_gato.colliderect(rect_peixe_dourado):
         posicao_peixe_douradoY = -100
-        posicao_peixe_douradoX = random.randint(0, 936)
-        pontos += 5 
+        posicao_peixe_douradoX = numero_aleatorio(0, 936)
+        pygame.mixer.Sound.play(efeito_pega_peixe)
+        pontos += 5
+    else:
+        posicao_peixe_douradoY = posicao_peixe_douradoY + velocidade_peixe_dourado
+        if  posicao_peixe_douradoY > 700:
+            posicao_peixe_douradoY = -100
+            posicao_peixe_douradoX = numero_aleatorio(0, 936)
     return posicao_peixe_douradoX, posicao_peixe_douradoY, pontos
 
-def atualiza_peixe(posicao_peixeX, posicao_peixeY, velocidade_peixe, pontos):
-    posicao_peixeY = posicao_peixeY + velocidade_peixe
-    if posicao_peixeY > 700:
+def atualiza_peixe(posicao_peixeX, posicao_peixeY, velocidade_peixe, pontos, rect_gato):
+    rect_peixe = pygame.Rect(posicao_peixeX, posicao_peixeY, 64, 64)
+    efeito_pega_peixe = pygame.mixer.Sound("recursos/efeitoPegarPeixe.mp3")
+    if rect_gato.colliderect(rect_peixe):
         posicao_peixeY = -100
-        posicao_peixeX = random.randint(0, 936)
-        pontos +=1
+        posicao_peixeX = numero_aleatorio(0, 936)
+        pygame.mixer.Sound.play(efeito_pega_peixe)
+        pontos += 1
+    else:   
+        posicao_peixeY = posicao_peixeY + velocidade_peixe
+        if posicao_peixeY > 700:
+            posicao_peixeY = -100
+            posicao_peixeX = numero_aleatorio(0, 936)
     return posicao_peixeX, posicao_peixeY, pontos    
 
-def atualiza_cogumelo(posicao_cogumeloX, posicao_cogumeloY, velocidade_cogumelo, pontos):
-    posicao_cogumeloY = posicao_cogumeloY + velocidade_cogumelo
-    if posicao_cogumeloY > 700:
+def atualiza_cogumelo(posicao_cogumeloX, posicao_cogumeloY, velocidade_cogumelo, pontos, rect_gato):
+    rect_cogumelo = pygame.Rect(posicao_cogumeloX, posicao_cogumeloY, 64, 64)
+    efeito_pega_cogum = pygame.mixer.Sound("recursos/efeitoPegarCogum..mp3")
+    if rect_gato.colliderect(rect_cogumelo):
         posicao_cogumeloY = -100
-        posicao_cogumeloX = random.randint(0, 936)
-        pontos -= 1
+        posicao_cogumeloX = numero_aleatorio(0, 936)
+        pygame.mixer.Sound.play(efeito_pega_cogum)
+        pontos -= 5
+    else:
+        posicao_cogumeloY = posicao_cogumeloY + velocidade_cogumelo
+        if posicao_cogumeloY > 700:
+            posicao_cogumeloY = -100
+            posicao_cogumeloX = numero_aleatorio(0, 936)
     return posicao_cogumeloX, posicao_cogumeloY, pontos
 
-def atualiza_cogumelo_verde(posicao_cogumelo_verdeX, posicao_cogumelo_verdeY, velocidade_cogumelo_verde, vidas):
-    posicao_cogumelo_verdeY = posicao_cogumelo_verdeY + velocidade_cogumelo_verde
-    if posicao_cogumelo_verdeY > 700:
+def atualiza_cogumelo_verde(posicao_cogumelo_verdeX, posicao_cogumelo_verdeY, velocidade_cogumelo_verde, vidas, rect_gato):
+    rect_cogumelo_verde = pygame.Rect(posicao_cogumelo_verdeX, posicao_cogumelo_verdeY, 64,64)
+    efeito_pega_cogum = pygame.mixer.Sound("recursos/efeitoPegarCogum..mp3")
+    if rect_gato.colliderect (rect_cogumelo_verde):
         posicao_cogumelo_verdeY = -100
-        posicao_cogumelo_verdeX = random.randint(0, 936)
+        posicao_cogumelo_verdeX = numero_aleatorio(0, 936)
+        pygame.mixer.Sound.play(efeito_pega_cogum)
         vidas -= 1
+    else:
+        posicao_cogumelo_verdeY = posicao_cogumelo_verdeY + velocidade_cogumelo_verde
+        if posicao_cogumelo_verdeY > 700:
+            posicao_cogumelo_verdeY = -100
+            posicao_cogumelo_verdeX = numero_aleatorio(0, 936)
     return posicao_cogumelo_verdeX, posicao_cogumelo_verdeY, vidas
--- main.py
+++ main.py
@@ -12,11 +12,6 @@ relogio = pygame.time.Clock()
 tela = pygame.display.set_mode( tamanho ) 
 pygame.display.set_caption("Cat's Dream")
 
-frame_index = 0
-gato_velocidade = 20
-direcao = "parado"
-contador_animacao_gato = 0
-velocidade_animacao_gato = 10  # A velocidade da animação (quanto menor, mais rápido)
 cogumelo_verde = pygame.image.load("recursos/cogumelo 2.png")
 cogumelo_verde = pygame.transform.scale(cogumelo_verde, (64, 64))  # Redimensiona o cogumelo verde
 peixe = pygame.image.load("recursos/peixe.png")
@@ -40,20 +35,40 @@ branco = (255,255,255)
 lareira =  [
    pygame.transform.scale(pygame.image.load("recursos/lareira 1.png"),(426, 435)),
    pygame.transform.scale(pygame.image.load("recursos/lareira 2.png"), (426, 435))]
-contador_lareira = 0
-frame_lareira = 0
-srprite_lareira = 20
-posicao_lareiraX = 605
-posicao_lareiraY = 300
-gato_y = 463
-gato_x = 100
 
 meow = pygame.mixer.Sound("recursos/meow.mp3")
-meow.set_volume(1.0)  # 80% do volume
+meow.set_volume(2.0)  
+efeito_pega_peixe = pygame.mixer.Sound("recursos/efeitoPegarPeixe.mp3")
+efeito_pega_cogum = pygame.mixer.Sound("recursos/efeitoPegarCogum..mp3")
 pygame.mixer.music.load("recursos/trilhaSonora.mp3")
 pygame.mixer.music.play(loops=-1)
-pygame.mixer.music.set_volume(0.2)  # 30% do volume
+pygame.mixer.music.set_volume(0.2) 
 
+abajur = [
+    pygame.transform.scale(pygame.image.load("recursos/abajur sprites1.png"),(250, 600)),
+    pygame.transform.scale(pygame.image.load("recursos/abajur sprites2.png"),(250, 600)),
+    pygame.transform.scale(pygame.image.load("recursos/abajur sprites3.png"),(250, 600)),
+    pygame.transform.scale(pygame.image.load("recursos/abajur sprites4.png"),(250, 600))]
+
+contador_abajur = 0
+frame_abajur = 0
+sprite_abajur = 3
+posicao_abajurX = 0
+posicao_abajurY = 140
+
+contador_lareira = 0
+frame_lareira = 0
+sprite_lareira = 20
+posicao_lareiraX = 605
+posicao_lareiraY = 300
+
+gato_y = 463
+gato_x = 100
+frame_gato = 0
+gato_velocidade = 20
+direcao = "parado"
+contador_animacao_gato = 0
+velocidade_animacao_gato = 10  # A velocidade da animação (quanto menor, mais rápido)
 
 posicao_peixeX = 100
 posicao_peixeY = -100
@@ -67,17 +82,15 @@ posicao_cogumelo_verdeX = 400
 posicao_cogumelo_verdeY = -100
 velocidade_cogumelo_verde = 25
 
-
+posicao_peixe_douradoX = 300
+posicao_peixe_douradoY = -100
+velocidade_peixe_dourado = 25
 
 fonte_vidas = pygame.font.SysFont('Arial ',60)
 fonte_pontos = pygame.font.SysFont('Arial ',60)
 coracao = pygame.transform.scale(pygame.image.load("recursos/coracao.png"),(68, 68))
 vidas = 7
 pontos = 0
-
-posicao_peixe_douradoX = 300
-posicao_peixe_douradoY = -100
-velocidade_peixe_dourado = 25
 
 while True:
     for evento in pygame.event.get():
@@ -98,8 +111,6 @@ while True:
     else:
         direcao = "parado"
     
-   
-
     if gato_x < 0:
         gato_x = 0
     elif gato_x > 850:
@@ -108,35 +119,52 @@ while True:
     contador_animacao_gato += 1
     if contador_animacao_gato >= velocidade_animacao_gato:
         contador_animacao_gato = 0
-        frame_index = (frame_index + 1) % 2  # alterna entre 0 e 1
+        frame_gato = (frame_gato + 1) % 2  # alterna entre 0 e 1
     if direcao == "direita":
-        imagem_gato = gato_andando_esquerda[frame_index]
+        imagem_gato = gato_andando_esquerda[frame_gato]
     elif direcao == "esquerda":
-        imagem_gato = gato_andando_direita[frame_index]
+        imagem_gato = gato_andando_direita[frame_gato]
     else:
         imagem_gato = gato_parado
 
     contador_lareira += 1
-    if contador_lareira >= srprite_lareira:
+    if contador_lareira >= sprite_lareira:
         contador_lareira = 0
         frame_lareira += 1
         if frame_lareira >= len(lareira):
             frame_lareira = 0
-  
-    posicao_peixeX, posicao_peixeY, pontos = atualiza_peixe(posicao_peixeX, posicao_peixeY, velocidade_peixe, pontos)
+    
+    contador_abajur += 1
+    if contador_abajur >= sprite_abajur:
+        contador_abajur = 0
+        frame_abajur += 1
+        if frame_abajur >= len(abajur):
+            frame_abajur = 0
+
+    rect_gato = pygame.Rect(gato_x + 30, gato_y + 100 , 110, 330) 
+    rect_peixe = pygame.Rect(posicao_peixeX, posicao_peixeY, 64, 64)
+    rect_peixe_dourado = pygame.Rect(posicao_peixe_douradoX, posicao_peixe_douradoY, 64, 64)
+    rect_cogumelo = pygame.Rect(posicao_cogumeloX, posicao_cogumeloY, 64, 64)
+    rect_cogumelo_verde = pygame.Rect(posicao_cogumelo_verdeX, posicao_cogumelo_verdeY, 64,64)
+
+    posicao_peixeX, posicao_peixeY, pontos = atualiza_peixe(posicao_peixeX, posicao_peixeY, velocidade_peixe, pontos, rect_gato)
     
     posicao_peixe_douradoX, posicao_peixe_douradoY, pontos = atualiza_peixe_dourado(posicao_peixe_douradoX, posicao_peixe_douradoY, 
-    velocidade_peixe_dourado, pontos)
+    velocidade_peixe_dourado, pontos, rect_gato)
 
-    posicao_cogumeloX, posicao_cogumeloY, pontos = atualiza_cogumelo(posicao_cogumeloX, posicao_cogumeloY, velocidade_cogumelo, pontos)
+    posicao_cogumeloX, posicao_cogumeloY, pontos = atualiza_cogumelo(posicao_cogumeloX, posicao_cogumeloY, velocidade_cogumelo, pontos, rect_gato)
 
     posicao_cogumelo_verdeX, posicao_cogumelo_verdeY, vidas = atualiza_cogumelo_verde(posicao_cogumelo_verdeX, posicao_cogumelo_verdeY,
-    velocidade_cogumelo_verde, vidas)
+    velocidade_cogumelo_verde, vidas, rect_gato)
+    
+    if pontos < 0:
+        pontos = 0
+        vidas -= 1
     
 
-    
     tela.fill(branco)
     tela.blit(fundo, (0, 0))  # Desenha o fundo
+    tela.blit(abajur[frame_abajur], (posicao_abajurX, posicao_abajurY))
     tela.blit(lareira[frame_lareira], (posicao_lareiraX , posicao_lareiraY))  # Desenha a lareira na posição (500, 200)
     tela.blit(coracao, (10, 20))
     tela.blit(peixe, (posicao_peixeX, posicao_peixeY))  # Desenha o peixe na posição (100, 100)
